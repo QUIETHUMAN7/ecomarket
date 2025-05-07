ac646b8b68495c9d4a1f1a9285f908ecab92f278
Melipilla <melipilla@duoc.acad>
2025-05-07 14:47:15 -0400

    creacion de service

---
+++ src/main/java/com/eckomarket/cl/eckomarket/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.eckomarket.cl.eckomarket.repository;
+
+
+import com.eckomarket.cl.eckomarket.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+}
---
+++ src/main/java/com/eckomarket/cl/eckomarket/service/UsuarioService.java
@@ -0,0 +1,36 @@
+package com.eckomarket.cl.eckomarket.service;
+
+import com.eckomarket.cl.eckomarket.model.Usuario;
+import com.eckomarket.cl.eckomarket.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(long id) {
+        return usuarioRepository.findById(id).get();
+
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+
+    }
+
+    public void delete(Long id) {
+        usuarioRepository.deleteById(id);
+
+    }
+}
